7db5e281f998ca6c6323e3f29b04687268f33867
Matt Kustes <mattkustes@gmail.com>
2024-01-24 11:11:20 -0500

    google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { FrontContextProvider } from "./providers/frontContext";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-9FCBZSPY3M");
 
 ReactDOM.render(
   <React.StrictMode>
